4cf9b83b45026ac8c18a9a64321073cd299b866d
Vlad w60047 <vladhusaiov@gmail.com>
2021-03-08 21:32:04 +0100

    Added: API Skeleton

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,35 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext context;
+
+        public ActivitiesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await context.Activities.FindAsync(id);
+        }
+    }
+}
